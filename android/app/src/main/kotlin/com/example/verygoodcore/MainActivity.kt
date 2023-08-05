2eb1165dd0190fe647141c77c1f03aed7a9f8016
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2023-08-05 09:36:11 +0530

    Very good Setup

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.find_people_near_me
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:find_people_near_me/app/app.dart';
+import 'package:find_people_near_me/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
